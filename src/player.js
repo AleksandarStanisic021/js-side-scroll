6a98e44af3081abdd2b425c9ba5daa567daa9dcc
ciandre1976 <ciandre23@gmail.com>
2026-03-10 20:42:53 +0100

    Update player class to set initial position and add image rendering

--- src/player.js
+++ src/player.js
@@ -11,13 +11,22 @@ export class Player {
         this.width = 100;
         this.height = 91.3;
         this.x = 0;
-        this.y = 100;
+        this.y = this.game.height - this.height;
+        this.image = dogImage;
     }
 
     update() { }
 
     draw(context) {
+        context.fillStyle = 'red';
         context.fillRect(this.x, this.y, this.width, this.height);
+        context.drawImage(this.image,
+            0, 0,
+            this.width,
+            this.height,
+            0, this.game.height - this.height,
+            this.width,
+            this.height);
     }
 
 }
